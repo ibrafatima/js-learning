f6ca5380136ca1ddbcf3cd88d1cbd0e86e918309
ibra fatima <147346072+ibrafatima@users.noreply.github.com>
2026-02-26 22:12:16 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,5 @@
+accountid= "hc.com"
+accountpass= "5666"
+accountCity= "delhi"
+
+console.table([accountid ,accountpass ,accountCity])
---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat code as modern JavaScript
+
+
+// null is a special value that represents "no value" or "empty value". 
+// It is often used to indicate that a variable has no value or that an 
+// object property is missing.
+
+// undefined is a special value that represents "undefined value". It is
+// often used to indicate that a variable has not been assigned a value or 
+// that a function does not return a value.
+
+// symbol is a primitive data type that represents a unique identifier. 
+// It is often used to create unique property keys for objects or to
+// create unique values for constants.
+
+console.log(typeof null); // "object;
+console.log(typeof undefined); // "undefined"
+console.log(typeof Symbol("id")); // "symbol"
